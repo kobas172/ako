8e119f76958438ddd4f3f452a336020a587b3a37
kobas <60666561+kobas172@users.noreply.github.com>
2022-01-04 17:56:10 +0100

    Add files via upload

---
+++ lab6/zad2.asm
@@ -0,0 +1,146 @@
+; Program gwiazdki.asm
+; Wyœwietlanie znaków * w takt przerwañ zegarowych
+; Uruchomienie w trybie rzeczywistym procesora x86
+; lub na maszynie wirtualnej
+; zakoñczenie programu po naciœniêciu klawisza 'x'
+; asemblacja (MASM 4.0): masm gwiazdki.asm,,,;
+; konsolidacja (LINK 3.60): link gwiazdki.obj;
+.386
+rozkazy SEGMENT use16
+ASSUME CS:rozkazy
+;============================================================
+; procedura obs³ugi przerwania zegarowego
+obsluga_zegara PROC
+; przechowanie u¿ywanych rejestrów
+	push ax
+	push bx
+	push es
+; wpisanie adresu pamiêci ekranu do rejestru ES - pamiêæ
+; ekranu dla trybu tekstowego zaczyna siê od adresu B8000H,
+; jednak do rejestru ES wpisujemy wartoœæ B800H,
+; bo w trakcie obliczenia adresu procesor ka¿dorazowo mno¿y
+; zawartoœæ rejestru ES przez 16
+	mov bl, cs:przerwanie
+	inc bl
+	mov cs:przerwanie, bl
+
+	mov ax, 0B800h ;adres pamiêci ekranu
+	mov es, ax
+; zmienna 'licznik' zawiera adres bie¿¹cy w pamiêci ekranu
+	mov bx, cs:licznik
+; przes³anie do pamiêci ekranu kodu ASCII wyœwietlanego znaku
+; i kodu koloru: bia³y na czarnym tle (do nastêpnego bajtu)
+	mov byte PTR es:[bx], '*' ; kod ASCII
+	mov byte PTR es:[bx+1], 00000111B ; kolor
+; zwiêkszenie o 2 adresu bie¿¹cego w pamiêci ekranu
+	add bx,2
+; sprawdzenie czy adres bie¿¹cy osi¹gn¹³ koniec pamiêci ekranu
+	cmp bx,4000
+	jb wysw_dalej ; skok gdy nie koniec ekranu
+; wyzerowanie adresu bie¿¹cego, gdy ca³y ekran zapisany
+	mov bx, 0
+;zapisanie adresu bie¿¹cego do zmiennej 'licznik'
+	wysw_dalej:
+	mov cs:licznik,bx
+; odtworzenie rejestrów
+	pop es
+	pop bx
+	pop ax
+; skok do oryginalnej procedury obs³ugi przerwania zegarowego
+	jmp dword PTR cs:wektor8
+; dane programu ze wzglêdu na specyfikê obs³ugi przerwañ
+; umieszczone s¹ w segmencie kodu
+	licznik dw 0 ; wyœwietlanie pocz¹wszy od 2. wiersza
+	wektor8 dd ?
+	przerwanie db 0
+obsluga_zegara ENDP
+
+klawiatura PROC
+
+	in al, 60h
+	cmp al, 45
+	je ustaw
+	jmp brak
+ustaw:
+	mov cs:przerwij, byte PTR 1
+
+brak:
+	jmp dword PTR cs:wektor9
+	wektor9 dd ?
+	przerwij db 0
+klawiatura ENDP
+;============================================================
+; program g³ówny - instalacja i deinstalacja procedury
+; obs³ugi przerwañ
+; ustalenie strony nr 0 dla trybu tekstowego
+zacznij:
+mov al, 0
+    mov ah, 5
+    int 10
+    mov ax, 0
+    mov ds, ax
+
+    ; odczytanie zawartoci wektora nr 8 i zapisanie go w zmiennej 'wektor8'
+    ; (wektor nr 8 zajmuje w pamici 4 bajty poczwszy od adresu fizycznego 8 * 4 = 32) 
+    mov    eax,ds:[32]  ; adres fizyczny 0*16 + 32 = 32 
+    mov    cs:wektor8, eax   
+     
+    ; wpisanie do wektora nr 8 adresu procedury 'obsluga_zegara' 
+    mov    ax, SEG obsluga_zegara ; cz segmentowa adresu 
+    mov    bx, OFFSET obsluga_zegara  ; offset adresu 
+ 
+    cli    ; zablokowanie przerwa  
+ 
+    ; zapisanie adresu procedury do wektora nr 8 
+    mov    ds:[32], bx   ; OFFSET           
+    mov    ds:[34], ax   ; cz. segmentowa 
+ 
+    sti     
+
+    mov    eax,ds:[36] 
+    mov    cs:wektor9, eax   
+
+
+
+    mov    ax, SEG klawiatura    
+	mov    bx, OFFSET klawiatura 
+ 
+    cli   
+ 
+    mov  ds:[36], bx      
+    mov  ds:[38], ax   
+ 
+    sti 
+
+
+	aktywne_oczekiwanie:
+	cmp cs:przerwanie, 120
+	jae koniec
+	cmp cs:przerwij, 1
+	je koniec
+	jmp aktywne_oczekiwanie
+
+; deinstalacja procedury obs³ugi przerwania zegarowego
+; odtworzenie oryginalnej zawartoœci wektora nr 8
+koniec:
+    mov    eax, cs:wektor8 
+    cli 
+    mov    ds:[32], eax  ; przes³anie wartoci oryginalnej do 
+                         ; wektora 8 w tablicy wektorów przerwa 
+    sti 
+ 
+    ; odtworzenie oryginalnej zawartoci wektora nr 8 
+    mov    eax, cs:wektor9
+    cli 
+    mov    ds:[36], eax  ; przes³anie wartoci oryginalnej do 
+                         ;wektora 8 w tablicy wektorów przerwa 
+    sti 
+; zakoñczenie programu
+	mov al, 0
+	mov ah, 4CH
+	int 21H
+rozkazy ENDS
+nasz_stos SEGMENT stack
+db 128 dup (?)
+nasz_stos ENDS
+END zacznij
